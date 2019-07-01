48b11ecd779dc22bddc024621570003875a89e3f
Frey Hargarten <harga012@umn.edu>
2019-07-01 16:21:40 -0500

    more code commentary

--- app/dots.js
+++ app/dots.js
@@ -5,22 +5,27 @@ class Dots {
 
     //global variable declarations and function binding
     constructor(regl, donationsData) {
+        //assign the main tools we need for this: a regl instance and the JSON data
         this.regl = regl;
         this.donationsData = donationsData;
+
+        //dataviz display settings
         this.width = 1000;
         this.height = 1500;
         this.numPoints = donationsData.length;
         this.pointWidth = 3;
+        this.points = d3.range(this.numPoints).map(function (d) { return {}; }); //array of points to generate as dataviz sprites
+        this.colorScales = [d3.scaleSequential(d3.interpolateViridis), d3.scaleSequential(d3.interpolateMagma), d3.scaleSequential(d3.interpolateInferno), d3
+            .scaleSequential(d3.interpolateCool)].map(this._wrapColorScale);
+
+        //dataviz animation settions
         this.duration = 1000;
         this.delayByIndex = 500 / this.numPoints;
         this.maxDuration = this.duration + this.delayByIndex * this.numPoints;
         this.currentLayout = 0;
         this.currentColorScale = 0;
-        this.colorScales = [d3.scaleSequential(d3.interpolateViridis), d3.scaleSequential(d3.interpolateMagma), d3.scaleSequential(d3.interpolateInferno), d3
-            .scaleSequential(d3.interpolateCool)].map(this._wrapColorScale);
-        this.points = d3.range(this.numPoints).map(function (d) { return {}; });
-        // this.frag = fs.readFileSync(path.join(__dirname, 'frag.txt'), 'utf8'); //important vector math file
-        // this.vert = fs.readFileSync(path.join(__dirname, 'vert.txt'), 'utf8'); //important vector math file
+
+        //scoping 'this' keyword to private methods so sanity can reign
         this._buildPoints = this._buildPoints.bind(this);
         this._candidateColors = this._candidateColors.bind(this);
         this._wrapColorScale = this._wrapColorScale.bind(this);
@@ -42,15 +47,12 @@ class Dots {
 
     //REGL SPRITE CONSTRUCTION
     _buildPoints(points) {
-        
         var self = this;
 
-        //vector math to draw webgl sprites, in this case, a dot
+        //vector math to draw webgl sprites, in this case, a dot. messing this is – unwise – unless you know what you're doing
         var drawPoints = self.regl({
             frag: '\n\t\t  precision highp float;\n\t\t\tvarying vec3 fragColor;\n\t\t\tvoid main() {\n\t\t\t\tgl_FragColor = vec4(fragColor, 1);\n\t\t\t}\n\t\t\t',
             vert: '\n\t\t\tattribute vec2 positionStart;\n\t\t\tattribute vec2 positionEnd;\n\t\t\tattribute float index;\n\t\t\tattribute vec3 colorStart;\n\t\t\tattribute vec3 colorEnd;\n\n\t\t\tvarying vec3 fragColor;\n\n\t\t\tuniform float pointWidth;\n\t\t\tuniform float stageWidth;\n\t\t\tuniform float stageHeight;\n\t\t\tuniform float elapsed;\n\t\t\tuniform float duration;\n\t\t\tuniform float delayByIndex;\n\t\t\t// uniform float tick;\n\t\t\t// uniform float animationRadius;\n\t\t\tuniform float numPoints;\n\n\t\t\t// helper function to transform from pixel space to normalized device coordinates (NDC)\n\t\t\t// in NDC (0,0) is the middle, (-1, 1) is the top left and (1, -1) is the bottom right.\n\t\t\tvec2 normalizeCoords(vec2 position) {\n\t\t\t\t// read in the positions into x and y vars\n\t      float x = position[0];\n\t      float y = position[1];\n\n\t\t\t\treturn vec2(\n\t\t      2.0 * ((x / stageWidth) - 0.5),\n\t\t      // invert y since we think [0,0] is bottom left in pixel space\n\t\t      -(2.0 * ((y / stageHeight) - 0.5)));\n\t\t\t}\n\n\t\t\t// helper function to handle cubic easing (copied from d3 for consistency)\n\t\t\t// note there are pre-made easing functions available via glslify.\n\t\t\tfloat easeCubicInOut(float t) {\n\t\t\t\tt *= 2.0;\n        t = (t <= 1.0 ? t * t * t : (t -= 2.0) * t * t + 2.0) / 2.0;\n\n        if (t > 1.0) {\n          t = 1.0;\n        }\n\n        return t;\n\t\t\t}\n\n\t\t\tvoid main() {\n\t\t\t\tgl_PointSize = pointWidth;\n\n\t\t\t\tfloat delay = delayByIndex * index;\n\t      float t;\n\n\t      // drawing without animation, so show end state immediately\n\t      if (duration == 0.0) {\n\t        t = 1.0;\n\n\t      // still delaying before animating\n\t      } else if (elapsed < delay) {\n\t        t = 0.0;\n\t      } else {\n\t        t = easeCubicInOut((elapsed - delay) / duration);\n\t      }\n\n\t      // interpolate position\n\t      vec2 position = mix(positionStart, positionEnd, t);\n\n\t      // apply an ambient animation\n\t\t\t\t// float dir = index > numPoints / 2.0 ? 1.0 : -1.0;\n\t      // position[0] += animationRadius * cos((tick + index) * dir);\n\t      // position[1] += animationRadius * sin((tick + index) * dir);\n\n\t      // above we + index to offset how they move\n\t      // we multiply by dir to change CW vs CCW for half\n\n\n\t      // interpolate color\n\t      fragColor = mix(colorStart, colorEnd, t);\n\n\t      // scale to normalized device coordinates\n\t\t\t\t// gl_Position is a special variable that holds the position of a vertex\n\t      gl_Position = vec4(normalizeCoords(position), 0.0, 1.0);\n\t\t\t}\n\t\t\t',
-            // frag: self.frag,
-            // vert: self.vert,
             attributes: {
                 positionStart: points.map(function (d) {
                     return [d.sx, d.sy];
@@ -114,6 +116,8 @@ class Dots {
     }
 
     //SHAPE LAYOUTS
+
+    //big blue field shape
     _fieldSpill(points, width, height, donationsData) {
         var self = this;
 
@@ -189,12 +193,15 @@ class Dots {
         
     }
     
+    //bar chart shape
     _chartSpill(points, width, height, donationsData) {
         var self = this;
         
+        //point size and spacing for this shape
         var pointWidth = width / 800;
         var pointMargin = 1;
         
+        //chop up data into buckets of different values in provided column
         var bydonor_can = d3.nest()
             .key(function (d) {
                 return d.donor_can;
@@ -231,8 +238,10 @@ class Dots {
             })
             .object(binsArray);
         
+        //assign colors to our points
         self._candidateColors(points, donationsData);
         
+        //redraw the dots according to the categorized bins we've generated
             var arrangement = points.map(function (d, i) {
             var donor_can = donationsData[i].donor_can;
             var bin = bins[donor_can];
@@ -258,9 +267,11 @@ class Dots {
         });
     }
     
+    //map shape
     _mapSpill(points, width, height, donationsData) {
         var self = this;
 
+        //grab coordinates from our JSON data
             var latitude = d3.extent(donationsData, function (d) {
                 return d.lat
             });
@@ -268,6 +279,7 @@ class Dots {
                 return d.lng
             });
             
+        //transmute our coordinates into GEOJSON
             var extentGeoJson = {
                 type: "LineString", coordinates: [
                     [longitude[0], latitude[0]], 
@@ -275,17 +287,21 @@ class Dots {
                 ]
             };
     
+        //classic D3 mercator map projection magicks
             var projection = d3.geoMercator().fitSize([width, height], extentGeoJson);
                 
+        //assign each point a projected coordinate to place on the map
             points.forEach(function (d, i) {
                 var location = projection([donationsData[i].lng, donationsData[i].lat]);
                 d.x = location[0];
                 d.y = location[1];
             })
     
+        //color the map points
             self._candidateColors(points, donationsData);
     }
     
+    //zip code breakdown shape
     _zipSpill(points, pointWidth, xOffset, yOffset, donationsData) {
         var self = this;
 
@@ -314,6 +330,7 @@ class Dots {
         return points
     }
     
+    //donation totals chart shape
     _totalsSpill(points, width, height, donationsData) {
         var self = this;
 
@@ -393,6 +410,7 @@ class Dots {
         });
     }
     
+    //area chart timeline shape
     _areaSpill(points, width, height, donationsData) {
         var self = this;
 
@@ -431,6 +449,7 @@ class Dots {
         })
     }
 
+    //fadeout, banish those dots from the realm
     _fadeout(points) {
         points.forEach(function (d, i) {
             d.color = [255, 255, 255];
@@ -473,6 +492,7 @@ class Dots {
     _animate(layout, points) {
             var self = this;
 
+            //transmuting our points to new positions and colors
             points.forEach(function (d) {
                 d.sx = d.tx;
                 d.sy = d.ty;
@@ -491,6 +511,7 @@ class Dots {
             
             var drawPoints = self._buildPoints(points);
             
+            //animating the change
             var startTime = null;
             var frameLoop = self.regl.frame(function (d) {
                 var time = d.time;
