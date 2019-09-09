9d732082bd58016cc8326a43269993e23908fabe
Frey Hargarten <harga012@umn.edu>
2019-09-09 12:41:20 -0500

    updates

--- app/index.js
+++ app/index.js
@@ -13,36 +13,8 @@
 import utils from './shared/utils.js';
 import Dots from './dots.js'
 
-var datapoints = './data/donations_data.json';
+var dataArray = [];
 
-//data loader
-function dataSpill(source) {
-    return new Promise(function (load) {
-        function donationsJSON() {
-            var argumentCount = [],
-            length = arguments.length;
-            while (length--) {
-                argumentCount[length] = arguments[length];
-            }
-            return d3.json(argumentCount[0], argumentCount[1])
-        };
-        d3.queue()
-            .defer(donationsJSON, source)
-            .await(function (error, donationsData) {
-                load({
-                    donationsData: donationsData.donations
-                 });
-            });
-    });
-}
+const dots = new Dots('#chart',dataArray);
 
-dataSpill(datapoints)
-    .then(function (d) {
-        var donationsData = d.donationsData;
-        createREGL({
-            onDone: function onDone(err, regl) {
-                const launch = new Dots(regl, donationsData);
-                launch.render();
-            }
-        });
-    });
+dots.render();
